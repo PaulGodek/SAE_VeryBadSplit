8c0824914dfd09f743622a0b80442c5792a071e5
sombek <kilyan.sombe@etu.umontpellier.fr>
2025-06-04 21:56:08 +0200

    Problème sécu 6 et 7 ok

--- src/Modele/Repository/EvenementRepository.php
+++ src/Modele/Repository/EvenementRepository.php
@@ -14,9 +14,9 @@ class EvenementRepository
     private function recupererPar($critere, $valeur) : ?Evenement
     {
         $pdoStatement = ConnexionBaseDeDonnees::getPdo()->prepare(
-            "SELECT * FROM app_db WHERE $critere = '$valeur'"
+            "SELECT * FROM app_db WHERE critere = valeur"
         );
-        $pdoStatement->execute();
+        $pdoStatement->execute(["critere"=>$critere, "valeur"=>$valeur]);
 
         $data = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
         if(!$data) {
