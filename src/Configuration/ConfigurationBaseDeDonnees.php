e7fe7dca4ad6e40859b10608afb6890591892170
godekp <paul.godek@etu.umontpellier.fr>
2025-06-06 19:34:37 +0200

    Configuration Base de Donnée de retour

---
+++ src/Configuration/ConfigurationBaseDeDonnees.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\VeryBadSplit\Configuration;
+
+class ConfigurationBaseDeDonnees {
+
+    static private array $configurationBaseDeDonnees = array(
+        'nomHote' => 'https://webinfo.iutmontp.univ-montp2.fr',
+        'nomBaseDeDonnees' => 'godek',
+        'port' => '3316',
+        'login' => 'godekp',
+        'motDePasse' => '100793528EF'
+    );
+
+    static public function getLogin() : string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['login'];
+    }
+
+    static public function getNomBaseDeDonnees() : string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['nomBaseDeDonnees'];
+    }
+
+    static public function getPort() : string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['port'];
+    }
+
+    static public function getNomHote() : string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['nomHote'];
+    }
+
+    static public function getMotDePasse() : string {
+        return ConfigurationBaseDeDonnees::$configurationBaseDeDonnees['motDePasse'];
+    }
+
+}
