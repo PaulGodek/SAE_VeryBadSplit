877db170521c187c0be5eb282733982792f8296d
matteo dipasquale <matteo.di-pasquale@etu.umontpellier.fr>
2025-06-08 00:25:57 +0200

    This reverts commit 2d2ff443022e78ccc31d7cd08e2599313b0a878c.
    git push origin dev#

--- src/Controleur/RouteurURL.php
+++ src/Controleur/RouteurURL.php
@@ -3,19 +3,23 @@
 namespace App\VeryBadSplit\Controleur;
 
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\UrlHelper;
-use Symfony\Component\HttpKernel\Controller\ContainerControllerResolver;
 use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Loader\AttributeDirectoryLoader;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use App\VeryBadSplit\Lib\AttributeRouteControllerLoader;
 use App\VeryBadSplit\Lib\Conteneur;
+use App\VeryBadSplit\Modele\Repository\UtilisateurRepository;
+use App\VeryBadSplit\Modele\Repository\EvenementRepository;
+use App\VeryBadSplit\Modele\Repository\DepenseRepository;
+use App\VeryBadSplit\Service\UtilisateurService;
+use App\VeryBadSplit\Service\EvenementService;
+use App\VeryBadSplit\Service\DepenseService;
 
 class RouteurURL
 {
@@ -23,29 +27,13 @@ class RouteurURL
     {
         $requete = Request::createFromGlobals();
 
-        // Contexte de la requête
-        $contexteRequete = (new RequestContext())->fromRequest($requete);
-
-        // Creation du conteneur
-        $conteneur = new ContainerBuilder();
-        $loader = new YamlFileLoader($conteneur, new FileLocator(__DIR__."/../Configuration"));
-
-        $loader->load("conteneur.yml");
-        $conteneur->setParameter('project_root', __DIR__.'/../..');
-
         // Chargement des routes
         $fileLocator = new FileLocator(__DIR__);
         $attrClassLoader = new AttributeRouteControllerLoader();
         $routes = (new AttributeDirectoryLoader($fileLocator, $attrClassLoader))->load(__DIR__);
-
-        // Ajout des services au conteneur
-        $generateurUrl = new UrlGenerator($routes, $contexteRequete);
-        $assistantUrl = new UrlHelper(new RequestStack(), $contexteRequete);
-
-        $conteneur->set(UrlGenerator::class, $generateurUrl);
-
-        Conteneur::ajouterService("generateurUrl", $generateurUrl);
-        Conteneur::ajouterService("assistantUrl", $assistantUrl);
+        
+        // Contexte de la requête
+        $contexteRequete = (new RequestContext())->fromRequest($requete);
 
         // Association de l'URL à une route
         $associateurUrl = new UrlMatcher($routes, $contexteRequete);
@@ -53,11 +41,52 @@ class RouteurURL
         $requete->attributes->add($donneesRoute);
 
         // Résolution du contrôleur et des arguments
-        $resolveurDeControleur = new ContainerControllerResolver($conteneur);
+        $resolveurDeControleur = new ControllerResolver();
         $controleur = $resolveurDeControleur->getController($requete);
 
         $resolveurDArguments = new ArgumentResolver();
         $arguments = $resolveurDArguments->getArguments($requete, $controleur);
+        
+        // Ajout des services au conteneur
+        $generateurUrl = new UrlGenerator($routes, $contexteRequete);
+        $assistantUrl = new UrlHelper(new RequestStack(), $contexteRequete);
+
+        Conteneur::ajouterService("generateurUrl", $generateurUrl);
+        Conteneur::ajouterService("assistantUrl", $assistantUrl);
+        
+        // Initialisation des repositories
+        $utilisateurRepository = new UtilisateurRepository();
+        $evenementRepository = new EvenementRepository();
+        $depenseRepository = new DepenseRepository();
+        
+        // Ajout des repositories au conteneur
+        Conteneur::ajouterService("utilisateurRepository", $utilisateurRepository);
+        Conteneur::ajouterService("evenementRepository", $evenementRepository);
+        Conteneur::ajouterService("depenseRepository", $depenseRepository);
+        
+        // Initialisation des services avec injection de dépendances
+        $utilisateurService = new UtilisateurService(
+            $utilisateurRepository,
+            $evenementRepository,
+            $depenseRepository
+        );
+        
+        $evenementService = new EvenementService(
+            $evenementRepository,
+            $utilisateurRepository,
+            $depenseRepository
+        );
+        
+        $depenseService = new DepenseService(
+            $depenseRepository,
+            $utilisateurRepository,
+            $evenementService
+        );
+        
+        // Ajout des services au conteneur
+        Conteneur::ajouterService("utilisateurService", $utilisateurService);
+        Conteneur::ajouterService("evenementService", $evenementService);
+        Conteneur::ajouterService("depenseService", $depenseService);
 
         // Exécution du contrôleur
         call_user_func_array($controleur, $arguments);
